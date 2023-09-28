3cbb6fa9691107c06dd316e926adc0640f8c8115
lpite <67955038+lpite@users.noreply.github.com>
2023-09-28 11:38:00 +0300

    Update main.cpp

--- laba1/main.cpp
+++ laba1/main.cpp
@@ -15,14 +15,6 @@ struct Student{
     Result results[4];
 };
 
-void RW(Student& students){
-	ofstream file1("INFO.DAT");
-			if(file1.is_open()){
-			file1.write((char*)&students,sizeof(Student));
-	}
-	file1.close();
-}
-
 void RW(string studentsString){
 	ofstream file1("INFO.txt");
 	if(file1.is_open()){
@@ -32,86 +24,101 @@ void RW(string studentsString){
 
 }
 
-void RD(bool fuck){
+void RD(double text){
+cout << "i hate";
 	ifstream file1("INFO.txt");
 	string line;
 	if(file1.is_open()){
-	while (std::getline(file1, line))
+	while (getline(file1, line))
 	{
-				std::cout << line << std::endl;
+        cout << line << std::endl;
 	}
 	//file1 << studentsString << endl;
 	}
 
 }
 
-void RD(){
+void RW(const Student students[], int size, const string& filename) {
+    ofstream outFile(filename, ios::binary);
 
+    if (outFile.is_open()) {
+        // Write array of students to file
+        outFile.write(reinterpret_cast<const char*>(students), sizeof(Student) * size);
+        outFile.close();
+        cout << "Students data written to binary file successfully." << endl;
+    } else {
+        cerr << "Unable to open file: " << filename << endl;
+    }
+}
+
+void RD(const string& filename) {
+    ifstream inFile(filename, ios::binary);
+
+    if (inFile.is_open()) {
+        inFile.seekg(0, ios::end);
+        streampos fileSize = inFile.tellg();
+        inFile.seekg(0, ios::beg);
+
+        int numStudents = fileSize / sizeof(Student);
+
+        Student* students = new Student[numStudents];
+        inFile.read(reinterpret_cast<char*>(students), fileSize);
+
+        for (int i = 0; i < numStudents - 1; ++i) {
+					for (int j = 0; j < numStudents - i - 1; ++j) {
+					double averageMark = 0;
+					double nextAverageMark = 0;
+
+							for (int j1 = 0; j1 < 4; ++j1) {
+								averageMark += students[j].results[j1].mark;
+							}
+							averageMark /= 4;
+							if(j+1 <= numStudents - i - 1){
+								for (int j1 = 0; j1 < 4; ++j1) {
+									nextAverageMark += students[j+1].results[j1].mark;
+								}
+							}
+						nextAverageMark /= 4;
+						cout << "av = " << averageMark << " next av = " << nextAverageMark << endl;
+						if (averageMark < nextAverageMark) {
+
+										swap(students[j], students[j + 1]);
+						}
+					}
+				}
+        for (int i = 0; i < numStudents; ++i) {
+            cout << "Name: " << students[i].name << " "
+                 << "Surname: " << students[i].surname << " "
+                 << "Course: " << students[i].course << " "
+                 << "Group: " << students[i].group << endl;
+						double averageMark = 0;
+            for (int j = 0; j < 4; ++j) {
+								averageMark += students[i].results[j].mark;
+                cout << "Subject: " << students[i].results[j].subject << " "
+                     << "Mark: " << students[i].results[j].mark << endl;
+            }
+            averageMark /= 4;
+						cout << "Average mark:" << averageMark;
+            cout << endl;
+        }
+
+        inFile.close();
+    } else {
+        cerr << "Unable to open file: " << filename << endl;
+    }
 }
 
 int main()
 {
-    Student students[] = {{
-        "sasha",
-        "ne sasha",
-        '1',
-        "23423",
-        {
-					{
-						"math",
-						34
-					},
-					{
-						"math",
-						34
-					},
-					{
-						"math",
-						34
-					},
-					{
-						"math",
-						34
-					}
-        }
-    },{
-        "fef",
-        "pr",
-        1,
-        "23423",
-        {
-					{
-						"test",
-						33333333
-					},
-					{
-						"math",
-						31
-					},
-					{
-						"math",
-						32
-					},
-					{
-						"math",
-						12
-					}
-        }
-    }};
-		for(int i1 = 0; i1 < sizeof (students) / sizeof (Student) - 1;i1++){
-
-		}
-		for(int i = 0; i < sizeof (students) / sizeof (Student);i++){
-			Student student = students[i];
-			RW(student);
-			double averageMark = 0;
-			for(int j = 0; j < sizeof (student.results) / sizeof (Result);j++){
-					double mark = student.results[j].mark;
-					averageMark += mark;
-			}
-			averageMark = averageMark / 4;
-			cout << averageMark << endl;
-		}
-
+    Student students[] = {
+			{"sasha","ne sasha",'1',"23423",{{"math",34},{"math",34},{"math",34},{"math",34}}},
+			{"fef","pr",'5',"23423",{{"test",12},{"math",12},{"math",12},{"math",12}}},
+			{"fef","pr",'5',"23423",{{"test",31},{"math",31},{"math",31},{"math",31}}},
+			{"fef","pr",'5',"23423",{{"test",40},{"math",40},{"math",40},{"math",40}}},
+			{"fef","pr",'5',"23423",{{"test",26},{"math",26},{"math",26},{"math",26}}},
+			{"fef","pr",'5',"23423",{{"test",25},{"math",25},{"math",25},{"math",25}}},
+    };
+		RW(students, sizeof(students)/sizeof(Student), "studentsArray.bin");
+		RD("studentsArray.bin");
     return 0;
 }
